78be98f4ab42f453c24ead2eb0d298b5d910da61
damoregui <guido.damore@hotmail.com>
2025-08-20 15:09:53 -0300

    changing the invitations settings

--- src/invitations.js
+++ src/invitations.js
@@ -1,6 +1,6 @@
 export const INVITATIONS = {
-  CYM001: { count: 2, names: ["Cami"] },
-  CYM002: { count: 1, names: ["Cami", "Milton"] },
+  CYM001: { count: 1, names: ["Cami"] },
+  CYM002: { count: 2, names: ["Cami", "Milton"] },
   CYM003: { count: 3, names: ["Cami", "Milton", "Cin"] },
   CYM004: { count: 4, names: ["Cami", "Milton", "Cin", "Gui"] },
   CYM005: { count: 5, names: ["Cami", "Milton", "Cin","Cami", "Milton"] },
